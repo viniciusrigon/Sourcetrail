e680fc4c29d7743ba60e3ab170698899ff6e1033
mlangkabel <malte.langkabel@gmail.com>
2020-01-13 20:07:43 +0100

    test

--- src/test/UtilityGradleTestSuite.cpp
+++ src/test/UtilityGradleTestSuite.cpp
@@ -8,18 +8,31 @@
 #	include "utility.h"
 #	include "utilityGradle.h"
 
+#	include <iostream>
+
 TEST_CASE("gradle wrapper detects source directories of simple projects")
 {
-	std::vector<FilePath> result = utility::gradleGetAllSourceDirectories(
-		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project"), false);
+	try
+	{
+		std::vector<FilePath> result = utility::gradleGetAllSourceDirectories(
+			FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project"), false);
 
-	REQUIRE(utility::containsElement<FilePath>(
-		result,
-		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/main/java").makeAbsolute()));
+		REQUIRE(utility::containsElement<FilePath>(
+			result,
+			FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/main/java").makeAbsolute()));
 
-	REQUIRE(!utility::containsElement<FilePath>(
-		result,
-		FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/test/java").makeAbsolute()));
+		REQUIRE(!utility::containsElement<FilePath>(
+			result,
+			FilePath(L"data/UtilityGradleTestSuite/simple_gradle_project/src/test/java").makeAbsolute()));
+	}
+	catch (std::exception e)
+	{
+		std::cout << "exception: " << e.what << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "unknown exception" << std::endl;
+	}
 }
 
 TEST_CASE("gradle wrapper detects source and test directories of simple projects")
